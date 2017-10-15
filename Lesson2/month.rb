ead184bb32ada9ca88a740e515da6b3a52c25c23
d_anoshkin <anoshkin1@yandex.ru>
2017-10-15 19:22:48 +0500

    Lesson2: first commit

---
+++ Lesson2/month.rb
@@ -0,0 +1,20 @@
+months={                      #хэш с названием месяцев года в качестве ключа и количеством дней в качестве значения
+  january: 31,                #
+  february: 28,               #
+  march: 31,                  #
+  april: 30,                  #
+  may: 31,                    #
+  june: 30,                   #
+  july: 31,                   #
+  august: 31,                 #
+  september: 30,              #
+  october: 31,                #
+  november: 30,               #
+  december: 31
+}
+
+months.each do |month,days|   #перебираем хэш
+  if days==30                 #если число дней в месяе равно 30
+    puts month                #печатаем месяц
+  end
+end
