a331335cad500bf50f9276a0b36871d79481fca8
d_anoshkin <anoshkin1@yandex.ru>
2017-11-07 14:44:53 +0500

    third edition railway

--- Lesson4/main.rb
+++ Lesson4/main.rb
@@ -6,26 +6,26 @@ menu.show_menu
 
 controller = Controller.new
 
-def create_new_station #1
+def create_new_station(controller) #1
   puts "Введите название станций(списком через запятую или поодиночке):"
   name = gets.chomp.upcase
   controller.create_station(name)
   puts "#{controller.stations}"
 end
 
-def create_passenger_train! #2
+def create_passenger_train!(controller) #2
   puts "Введите название пассажирского поезда"
   name = gets.chomp.upcase
   controller.create_passenger_train(name)
 end
 
-def create_cargo_train! #21
+def create_cargo_train!(controller) #21
   puts "Введите название грузового поезда"
   name = gets.chomp.upcase
   controller.create_cargo_train(name)
 end
 
-def create_passenger_wagon! #22
+def create_passenger_wagon!(controller) #22
   puts "Ведите тип создаваемого вагона"
   type_wagon = gets.chomp.upcase
   puts "Введите количество создаваемых вагонов"
@@ -33,7 +33,7 @@ def create_passenger_wagon! #22
   controller.create_passenger_wagon(type_wagon, quantity)
 end
 
-def create_cargo_wagon! #23
+def create_cargo_wagon!(controller) #23
   puts "Ведите тип создаваемого вагона"
   type_wagon = gets.chomp.upcase
   puts "Введите количество создаваемых вагонов"
@@ -41,7 +41,7 @@ def create_cargo_wagon! #23
   controller.create_cargo_wagon(type_wagon, quantity)
 end
 
-def attach_wagons_to_train #24
+def attach_wagons_to_train(controller) #24
   puts "Введите название поезда, к которому прицепить вагоны"
   name_train = gets.chomp.upcase
   puts "Введите тип вагона"
@@ -49,7 +49,7 @@ def attach_wagons_to_train #24
   controller.attach_wagon!(name_train, type_wagon)
 end
 
-def detach_wagons_from_train #25
+def detach_wagons_from_train(controller) #25
   puts "Введите название поезда, от которого надо отцепить вагоны"
   name_train = gets.chomp.upcase
   puts "Введите тип вагона"
@@ -57,19 +57,19 @@ def detach_wagons_from_train #25
   controller.detach_wagon!(name_train, type_wagon)
 end
 
-def move_train_forard! #26
+def move_train_forard!(controller) #26
   puts "Введите название поезда из списка поездов"
   name = gets.chomp.upcase
   controller.move_train_forward(name)
 end
 
-def move_train_back! #27
+def move_train_back!(controller) #27
   puts "Введите название поезда из списка поездов"
   name = gets.chomp.upcase
   controller.move_train_back(name)
 end
 
-def create_new_route #3
+def create_new_route(controller) #3
   puts "Введите название маршрута"
   name_route = gets.chomp.upcase
   puts "Введите начальную станцию из списка станций"
@@ -80,31 +80,31 @@ def create_new_route #3
   puts "#{controller.route}"
 end
 
-def add_middle_station #31
+def add_middle_station(controller) #31
   puts "Для добавления введите промежуточную станцию из списка станций"
   middle_station = gets.chomp.upcase
   controller.add_station!(middle_station)
 end
 
-def remove_middle_station #32
+def remove_middle_station(controller) #32
   puts "Для удаления введите промежуточную станцию из списка станций"
   middle_station = gets.chomp.upcase
   controller.del_station!(middle_station)
 end
 
-def set_route_for_passenger_train #33
+def set_route_for_passenger_train(controller) #33
   puts "Введите название маршрута"
   name_route = gets.chomp.upcase
   controller.select_route_for_passenger_train(name_route)
 end
 
-def set_route_for_cargo_train #34
+def set_route_for_cargo_train(controller) #34
   puts "Введите название маршрута"
   name_route = gets.chomp.upcase
   controller.select_route_for_cargo_train(name_route)
 end
 
-def show_all_stations #35
+def show_all_stations(controller) #35
   controller.all_stations
 end
 
@@ -116,35 +116,35 @@ loop do                      #цикл непрерывного пользова
     when 4
       abort
     when 1
-      create_new_station
+      create_new_station(controller)
     when 2
-      create_passenger_train!
+      create_passenger_train!(controller)
     when 21
-      create_cargo_train!
+      create_cargo_train!(controller)
     when 22
-      create_passenger_wagon!
+      create_passenger_wagon!(controller)
     when 23
-      create_cargo_wagon!
+      create_cargo_wagon!(controller)
     when 24
-      attach_wagons_to_train
+      attach_wagons_to_train(controller)
     when 25
-      detach_wagons_from_train
+      detach_wagons_from_train(controller)
     when 26
-      move_train_forard!
+      move_train_forard!(controller)
     when 27
-      move_train_back!
+      move_train_back!(controller)
     when 3
-      create_new_route
+      create_new_route(controller)
     when 31
-      add_middle_station
+      add_middle_station(controller)
     when 32
-      remove_middle_station
+      remove_middle_station(controller)
     when 33
-      set_route_for_passenger_train
+      set_route_for_passenger_train(controller)
     when 34
-      set_route_for_cargo_train
+      set_route_for_cargo_train(controller)
     when 35
-      show_all_stations
+      show_all_stations(controller)
     else
       puts "Неверный пункт меню"
   end
