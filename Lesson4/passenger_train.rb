e4ea57079a1238373e1b66d6390be19336e69ac9
d_anoshkin <anoshkin1@yandex.ru>
2017-11-08 08:56:58 +0500

    fifth edition railway

--- Lesson4/passenger_train.rb
+++ Lesson4/passenger_train.rb
@@ -7,11 +7,10 @@ class PassengerTrain < Train    #класс для создания пассаж
   end
 
   def attach_wagon(wagon)
-    @wagons << wagon if @current_speed == 0 && wagon.class == PassengerWagon  #вагон прицепляется, если текущая скорость поезда равна 0,
-  end                                                                     # и если это пассажирский вагон
+    if wagon.class == PassengerWagon
+    super  #вагон прицепляется, если текущая скорость поезда равна 0,
+    end
+  end     # и если это пассажирский вагон
 
-  def detach_wagon(wagon)
-    @wagons.delete(wagon) if @current_speed == 0 && @wagons.length > 0 && wagon.class == PassengerWagon  #вагон отцепляется, если значение текущей скорости равно 0 и количество вагонов больше 0
-  end                                                                     # и если это пассажирский вагон
 
 end
